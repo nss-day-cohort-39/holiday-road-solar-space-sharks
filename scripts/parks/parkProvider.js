df8644f449fcc8605d67659c62e3eea20f9fe679
durrjp <durrjp1@gmail.com>
2020-03-26 14:25:17 -0500

    tidying up

--- scripts/parks/parkProvider.js
+++ scripts/parks/parkProvider.js
@@ -4,12 +4,10 @@ let parks = []
 export const getParks = () => {
     
 
-// fetch only Park Properties for Dropdown
     return fetch(`http://localhost:3000/parks`)
             .then(response => response.json())
             .then(parsedParks => {
                     parks = parsedParks
-                    console.table(parks)
             })
 }
 
