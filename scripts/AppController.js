02831920299b8cdcd46849e617d04895e248a804
heidijane <heidi@heidisprouse.com>
2020-04-01 13:42:52 -0500

    App controller now keeps track of the state of the page, shows the current component and hides others when state is changed based on click events

--- scripts/AppController.js
+++ scripts/AppController.js
@@ -3,21 +3,86 @@
 */
 const eventHub = document.querySelector('.container')
 
+/*
+List of possible app states
+- home:
+    target = .welcomeContainer
+- parkSelect:
+    target = .dropdownContainer--parks
+- eaterySelect:
+    target = .dropdownContainer--food
+- attractionSelect:
+    target = .dropdownContainer--attraction
+- myTrip:
+    target = .previewContainer
+- savedTripsList:
+    target = .savedTripsContainer
+*/
+
 //default page state
-let pageState = "home"
+export let pageState = "home"
 
 eventHub.addEventListener("newTripBtnWasClicked", event => {
     pageState = "parkSelect"
+    pageStateChanged()
 })
 
 eventHub.addEventListener("saveParkButtonClicked", event => {
     pageState = "eaterySelect"
+    pageStateChanged()
 })
 
 eventHub.addEventListener("saveFoodButtonClicked", event => {
     pageState = "attractionSelect"
+    pageStateChanged()
 })
 
 eventHub.addEventListener("saveAttractionButtonClicked", event => {
     pageState = "myTrip"
+    pageStateChanged()
 })
+
+eventHub.addEventListener("myTripButtonClicked", event => {
+    pageState = "myTrip"
+    pageStateChanged()
+})
+
+eventHub.addEventListener("mySavedTripsBtnWasClicked", event => {
+    pageState = "savedTripsList"
+    pageStateChanged()
+})
+
+const hideAllComponents = () => {
+    const componentArray = [
+        '.welcomeContainer',
+        '.dropdownContainer--parks',
+        '.dropdownContainer--food',
+        '.dropdownContainer--attraction',
+        '.previewContainer',
+        '.savedTripsContainer'
+    ]
+
+    componentArray.forEach(component => document.querySelector(component).classList.add("hidden"))
+}
+
+export const pageStateChanged = () => {
+    //add the "hidden" class to all components to reset the app state before running the logic
+    hideAllComponents()
+
+    if (pageState === "home") {
+        document.querySelector(".welcomeContainer").classList.remove("hidden")
+    } else if (pageState === "parkSelect") {
+        document.querySelector(".dropdownContainer--parks").classList.remove("hidden")
+    } else if (pageState === "eaterySelect") {
+        document.querySelector(".dropdownContainer--food").classList.remove("hidden")
+    } else if (pageState === "attractionSelect") {
+        document.querySelector(".dropdownContainer--attraction").classList.remove("hidden")
+    } else if (pageState === "myTrip") {
+        document.querySelector(".previewContainer").classList.remove("hidden")
+    } else if (pageState === "savedTripsList") {
+        document.querySelector(".savedTripsContainer").classList.remove("hidden")
+    } else {
+        //if page state isn't set default to home
+        document.querySelector(".welcomeContainer").classList.remove("hidden")
+    }
+}
