44e9fbec7866a3b6f86213d171548dd8bb6bdd76
Thomas Long <thomas@Thomass-MacBook-Pro.local>
2020-04-01 12:07:39 -0500

    refactored to account for a park with no image

--- scripts/parks/ParkPreview.js
+++ scripts/parks/ParkPreview.js
@@ -10,7 +10,7 @@ export const ParkPreview = (parkObject) => {
 
     return `
             <h2>${parkObject.fullName}</h2>
-            <img width = "200px" src="${parkObject.images[0].url}" alt="Photo of ${parkObject.fullName}" title="Photo of ${parkObject.fullName}">
+            ${parkObject.images.length !== 0 ? `<img width = "200px" src="${parkObject.images[0].url}" alt="Photo of ${parkObject.fullName}" title="Photo of ${parkObject.fullName}">` : `<p>${parkObject.description}</p>`}
             <div class="weather">${parkObject.latitude !== "" ? RenderWeather() : parkObject.weatherInfo}
             </div>`
             
