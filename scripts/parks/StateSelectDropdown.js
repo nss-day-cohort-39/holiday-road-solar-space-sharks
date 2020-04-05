4aebb150d4bff51d8ba3b0fbd446311aa8d6a6e3
heidijane <heidi@heidisprouse.com>
2020-04-05 14:51:04 -0500

    Deleted territories from the state list with no eateries or attractions

--- scripts/parks/StateSelectDropdown.js
+++ scripts/parks/StateSelectDropdown.js
@@ -1,15 +1,15 @@
 const states = [
-    'AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA',
-    'GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA',
-    'MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND',
-    'MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT',
-    'VT','VI','VA','WA','WV','WI','WY'
-   ]
+    'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA',
+    'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA',
+    'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+    'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+    'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
+]
 
 
 export const StateSelectDropdown = () => {
 
-    return `
+        return `
         <select id="stateSelectDropdown">
             <option value="0">Where to next?</option>
             ${states.map(state => {
